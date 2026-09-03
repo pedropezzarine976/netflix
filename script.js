586116f07c6eb3e9b448b1316dd2975b52001d84
PEDRO HENRIQUE SILVA PEZZARINE <52710294842@info.local>
2026-09-03 18:00:07 -0300

    flix

---
+++ script.js
@@ -0,0 +1,17 @@
+//const elemento = document.getElementById('filme-principal')
+
+//console.log(elemento)
+//console.log(elemento.tagName)
+
+function selecionarPorId(){
+
+const tituloFilme = document.getElementById('titulo-filme')
+
+console.log(tituloFilme)
+console.log(tituloFilme.tagName)
+console.log(tituloFilme.textContent)
+console.log(tituloFilme.id)
+
+tituloFilme.classList.add("elemento-selecionado")
+
+}                                                           
